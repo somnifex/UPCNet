f960401b6dd73622a3a41ae276d67b60c1aed861
HowieWood <98788152+somnifex@users.noreply.github.com>
2025-07-07 11:11:57 +0800

    Update UPCNet.sh

--- UPCNet.sh
+++ UPCNet.sh
@@ -9,7 +9,7 @@ serviceB=""
 
 DATE=`date +%Y-%m-%d-%H:%M:%S`
 echo "Start detecting network connection status......"
-ip_list="47.104.38.82218.30.118.6,61.241.129.110"
+ip_list="47.104.38.82,218.30.118.6,61.241.129.110"
 ips=$(echo "$ip_list" | tr ',' ' ')
 for ip in $ips
 do
